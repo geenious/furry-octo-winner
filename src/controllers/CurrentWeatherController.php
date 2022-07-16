7d415e3b76d5c16dd716a0151c0383c5c601fb6d
Jeff Irwin <jeff.irwin@live.com>
2022-07-15 23:10:02 -0500

    good response

---
+++ src/controllers/CurrentWeatherController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace simplygoodwork\weather\controllers;
+
+use Craft;
+use craft\web\Controller;
+use GuzzleHttp;
+use yii\web\BadRequestHttpException;
+use yii\web\Response;
+
+class CurrentWeatherController extends Controller
+{
+  public function actionGetWeather(): Response
+  {
+    $this->requireSiteRequest();
+
+    $client = new GuzzleHttp\Client();
+    $response = $client->request('GET', 'https://api.openweathermap.org/data/2.5/weather?lat=28.0268742&lon=-97.1158471&units=imperial&appid=f71ca27686d3b702743df02a72e6914b');
+    $responseBody = json_decode($response->getBody(), true);
+
+    return $this->asJson([$responseBody]);
+  }
+}
